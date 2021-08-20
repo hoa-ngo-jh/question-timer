bab8145cbf54aa6aafc0e1fc063b3087e3d12b0f
Hoa Ngo <hoa.ngo@journeyh.io>
2021-08-20 16:46:31 +0700

    Add multiple choices & change list data file
    Add render function for multiple choices. Now we can add 2 options of question type that are write the answer or choose the given answer.
    
    We can change the type of question by pass the 'list' property in init function.
    
    Move the data questions to data.js because the file index.js is a module, it should only contain code related to that module. Separating code and data is easier for people working on the same file, and also for people who will later work on it. If we not seperated then when the module has grown larger, and data can be added more, it will be difficult for developers working with this file.

---
+++ data.js
@@ -0,0 +1,116 @@
+const list = [
+  {
+    id: 0,
+    type: 'text_plain',
+    question: "What is your favorite football club?",
+    time: 7 
+  },
+  {
+    id: 1,
+    type: 'text_plain',
+    question: "Do you read manga?",
+    time: 5
+  },
+  {
+    id: 2,
+    type: 'text_plain',
+    question: "Do you want to travel at home or abroad?",
+    time: 8
+  },
+  {
+    id: 3,
+    type: 'text_plain',
+    question: "Which county would you choose to live in?",
+    time: 5
+  },
+  {
+    id: 4,
+    type: 'text_plain',
+    question: "Write 5 football club in the world",
+    time: 8
+  },
+  {
+    id: 5,
+    type: 'text_plain',
+    question: "Do you like to eat fish?",
+    time: 5
+  },
+  {
+    id: 6,
+    type: 'text_plain',
+    question: "Write some difference between summer and winter",
+    time: 5
+  },
+  {
+    id: 7,
+    type: 'text_plain',
+    question: "What is your favorite singer?",
+    time: 9
+  },
+];
+
+const list2 = [
+  {
+    id: 0,
+    type: 'multiple_choices',
+    question: 'What is Australia capital?',
+    time: 5,
+    answer: 'Canberra',
+    choices: [
+      'Sydney',
+      'Adelaide',
+      'Canberra',
+      'Melbourne'
+    ]
+  },
+  {
+    id: 1,
+    type: 'multiple_choices',
+    question: 'Who is the author of Sherlock Holmes?',
+    time: 5,
+    answer: 'Arthur Conan Doyle',
+    choices: [
+      'Arthur Conan Doyle',
+      'Higashino Keigo',
+      'Agatha Christie',
+      'Thomas Harris'
+    ]
+  },
+  {
+    id: 2,
+    type: 'multiple_choices',
+    question: 'One Piece is a manga from which country?',
+    time: 5,
+    answer: 'Japan',
+    choices: [
+      'Korea',
+      'Viet Nam',
+      'USA',
+      'Japan'
+    ]
+  },
+  {
+    id: 3,
+    type: 'multiple_choices',
+    question: 'Who is the winner of the oscar award for best actor in 2016?',
+    time: 5,
+    answer: 'Leonardo DiCaprio',
+    choices: [
+      'Leonardo DiCaprio',
+      'Matthew McConaughey',
+      'Eddie Redmayne'
+    ]
+  },
+  {
+    id: 4,
+    type: 'multiple_choices',
+    question: 'Which football club is not in Europe?',
+    time: 5,
+    answer: 'Santos',
+    choices: [
+      'Santos',
+      'Barcelona',
+      'Manchester City'
+    ]
+  }
+];
--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-const list = [
-  {
-    id: 0,
-    question: "What is your favorite football club?",
-    time: 7 
-  },
-  {
-    id: 1,
-    question: "Do you read manga?",
-    time: 5
-  },
-  {
-    id: 2,
-    question: "Do you want to travel at home or abroad?",
-    time: 8
-  },
-  {
-    id: 3,
-    question: "Which county would you choose to live in?",
-    time: 5
-  },
-  {
-    id: 4,
-    question: "Write 5 football club in the world",
-    time: 8
-  },
-  {
-    id: 5,
-    question: "Do you like to eat fish?",
-    time: 5
-  },
-  {
-    id: 6,
-    question: "Write some difference between summer and winter",
-    time: 5
-  },
-  {
-    id: 7,
-    question: "What is your favorite singer?",
-    time: 9
-  },
-];
 
 const timerQuestion = (function () {
+  const TEXT_PLAIN_QUESTION = 'text_plain';
+  const MULTIPLE_CHOICES_QUESTION = 'multiple_choices';
+
   const timerConfig = {
     timerAppControl: null,
     timerLimitQuestion: null,
@@ -60,12 +21,26 @@ const timerQuestion = (function () {
   };
 
   const changeQuestion = (time, index, answer) => {
-    renderQuesitonBox(index, answer);
+    if (questions[index].type === TEXT_PLAIN_QUESTION) {
+      renderTextPlainQuesitonBox(index, answer);
+    } else if (questions[index].type === MULTIPLE_CHOICES_QUESTION) {
+      renderMultipleChoicesQuestionBox(index, answer);
+    }
+
     updateTimer(time, '-sub');
     countDownTimer(time, index);
   };
 
-  const renderQuesitonBox = (index, answer = '') => {
+  const setEventForDirectionButton = (currentQuestion, index) => {
+    document.querySelector('.wrapper').innerHTML = currentQuestion;
+    document.querySelector('.next-btn').addEventListener('click', nextQuestion);
+
+    if (index !== 0) {
+      document.querySelector('.back-btn').addEventListener('click', prevQuesiton);
+    }
+  };
+
+  const renderTextPlainQuesitonBox = (index, answer = '') => {
     const currentQuestion = `
     <div class="box">
       <div class="box-info">
@@ -87,13 +62,51 @@ const timerQuestion = (function () {
     </div>
     `;
   
-    document.querySelector('.wrapper').innerHTML = currentQuestion;
-    document.querySelector('.next-btn').addEventListener('click', nextQuestion);
-
-    if (index !== 0) {
-      document.querySelector('.back-btn').addEventListener('click', prevQuesiton);
-    }
+    setEventForDirectionButton(currentQuestion, index);
   }; 
+
+  const renderMultipleChoicesQuestionBox = (index, answer = '') => {
+    const choices = questions[index].choices.map((item, i) => {
+      if (answer === item) {
+        return `
+          <div class="choice">
+            <input type="checkbox" name="answer-choice" class="answer-choice" id="choice-${index}-${i}" value="${item}" checked>
+            <label for="0">${item}</label>
+          </div>
+        `;
+      } else {
+        return `
+          <div class="choice">
+            <input type="checkbox" name="answer-choice" class="answer-choice" id="choice-${index}-${i}" value="${item}">
+            <label for="0">${item}</label>
+          </div> 
+        `;
+      }
+    }).join('');
+
+    const currentQuestion = `
+    <div class="box">
+      <div class="box-info">
+        <div class="question">
+          <p>${questions[index].question}</p>
+        </div>
+        <div class="timer">
+          <div class="timer-minutes-sub">00</div>
+          <div class="">:</div>
+          <div class="timer-seconds-sub">00</div>
+        </div>
+      </div>
+      ${choices}
+    </div>
+
+    <div class="direction-btn">
+      ${index === 0 ? '' : '<button class="btn back-btn">Back</button>'}
+      <button class="btn next-btn">${index === questions.length - 1 ? 'Submit' : 'Next'}</button>
+    </div>
+    `; 
+
+    setEventForDirectionButton(currentQuestion, index);
+  };
 
   const hideElement = (element) => {
     document.querySelector(element).classList.add('hide');
@@ -148,10 +161,12 @@ const timerQuestion = (function () {
 
   const nextQuestion = () => {
     stopCountTimer(timerConfig.timerLimitQuestion);
-    updateCurrentAnswer(currentIndex);
+    if (questions[0].type === TEXT_PLAIN_QUESTION) {
+      updateCurrentAnswer(currentIndex);
+    } 
 
     if (isLastQuestion(currentIndex)) {
-      if (!isTimeout(timerConfig.timerRealtime)) {
+      if (isTimeout(timerConfig.timerRealtime)) {
         return;
       }
 
@@ -176,6 +191,22 @@ const timerQuestion = (function () {
     return element.value;
   };
 
+  const handleAddMultipleChoicesAnswer = (event) => {
+    const choice = event.target;
+    const choiceCheckboxes =  document.getElementsByName('answer-choice');
+
+    choiceCheckboxes.forEach(item => {
+      if (item !== choice) {
+        item.checked = false;
+      }
+    });
+
+    if (choice.classList == 'answer-choice') {
+      const answer = choice.getAttribute('value');
+      addAnswerToList(answer, currentIndex);
+    }
+  };
+
   const addAnswerToList = (answer, index) => {
     const ans = {
       questionId: questions[index].id,
@@ -190,7 +221,7 @@ const timerQuestion = (function () {
     const ans = getInputAnswer(document.getElementById('answer'));
     addAnswerToList(ans, index);
   };
-  
+
   const renderAnswerQuestions = (data) => {
     const html = data.map(item => `
       <div class="box">
@@ -211,19 +242,48 @@ const timerQuestion = (function () {
     document.querySelector('.wrapper').innerHTML = html;
   };
 
+  const renderMultilpleChoicesAnswerQuestions = (data) => {
+    const html = data.map(item => `
+      <div class="box">
+        <div class="result">
+          <div class="box-info">
+            <div class="question">
+              <p>Q: ${questions[item.questionId].question}</p>
+            </div>
+          </div>
+          <div>
+            <p>${isCorrectAnswer(item) ? 'Correct: ' + item.answer : 'Wrong, the answer must be ' + questions[item.questionId].answer}</p>
+          </div>
+        </div>
+        
+      </div>
+    `).join('');
+
+    document.querySelector('.wrapper').innerHTML = html;
+  };
+
   const handleSubmitAnswer = () => {
     stopCountTimer(timerConfig.timerAppControl);
-    renderAnswerQuestions(answers);
+    if (questions[0].type === TEXT_PLAIN_QUESTION) {
+      renderAnswerQuestions(answers);
+    } else if (questions[0].type === MULTIPLE_CHOICES_QUESTION) {
+      renderMultilpleChoicesAnswerQuestions(answers);
+    }
   };
 
   const sumTimePerQuestion = (questionList, prop) => {
     return questionList.reduce((prev, curr) => prev + curr[prop], 0);
   };
 
+  const isCorrectAnswer = (answer) => {
+    return answer.answer === questions[answer.questionId].answer;
+  };
+
   const init = (data) => {
     questions = data.list;
     currentIndex = data.startIndexQuestion;
     document.querySelector(data.startButton).addEventListener('click', () => start(data));
+    document.addEventListener('click', handleAddMultipleChoicesAnswer);
   };
 
   return {
@@ -232,7 +292,7 @@ const timerQuestion = (function () {
 })();
 
 timerQuestion.init({
-  list,
+  list: list2,
   startButton: '#start-btn',
   startIndexQuestion: 0,
   startTime: 0
